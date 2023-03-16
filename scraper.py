56414abc31104ef6350d9c63c220de496db1e07f
Maximilian H <lorganan@gmail.com>
2023-03-16 17:05:28 +0100

    fix scraper, scraping complete

--- scraper.py
+++ scraper.py
@@ -1,8 +1,14 @@
 from instaloader import Instaloader, Profile
+from glob import glob
 import time
-import os
+from os import path
 
-path = os.path.dirname(__file__)
+path = path.dirname(__file__)
+accounts_done = []
+for file in glob(path + "/*"):
+    filename = file.split("\\")[-1]
+    if not "." in filename:
+        accounts_done.append(filename)
 with open(path + "/accounts.txt", "r") as infile:
     lines = infile.read()
 accounts = lines.split("\n")
@@ -10,24 +16,24 @@ accounts = lines.split("\n")
 l = Instaloader(download_videos = False, 
                 save_metadata = False, 
                 post_metadata_txt_pattern="", 
-                dirname_pattern=path + "{profile}/")
+                dirname_pattern=path + "/{profile}/")
 
 def account_scraper(account):
     print(account)
     profile = Profile.from_username(l.context, account)
     posts = profile.get_posts()
-    post_counter = 0
+    post_counter = 1
     while True:
         for post in posts: 
+            print(post_counter)
             l.download_post(post, profile)
             post_counter += 1
             time.sleep(6)
-            print(post_counter)
-            if post_counter == 10:
-                print("break")
+            if post_counter > 10:
                 posts = []
                 break
         break
         
 for account in accounts:
-    account_scraper(account)
+    if account not in accounts_done:
+        account_scraper(account)
